b8d6461c9f67f72ad0b019ce3a0aecc0ec7bf3f1
ChrisBAshton <chrisashtonweb@gmail.com>
2016-10-01 07:23:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# GitHook Giphy
+Automatically add animated gifs to your PRs.
+
+## Usage
+Just make sure you include `{giphy "search term"}` in your PR somewhere.
+
+Example PR body:
+
+```
+This PR adds the foo to the bar.
+
+{giphy "awesome"}
+
+Testing:
+
+* `mocha`
+* etc...
+```
+
+This webhook is called whenever a PR is opened or edited. Not happy with your gif the first time around? Just edit your PR and add `{giphy "search term"}` again!
+
+## Limitations
+Currently only supports 1 `{giphy}` call per PR.
+
+Currently uses the [public API key](https://github.com/Giphy/GiphyAPI), which is rate-limited.
+
+------
+*Powered by Giphy*
---
+++ gif.js
@@ -0,0 +1,52 @@
+module.exports = function (data, process) {
+    var PAYLOAD      = data.payload,
+        prWasEdited  = PAYLOAD.action === 'opened' || PAYLOAD.action === 'edited',
+        wantsGif     = PAYLOAD.pull_request.body.match(/{giphy "(.+)"}/);
+
+    if (prWasEdited && wantsGif) {
+
+        var searchTerm = encodeURIComponent(wantsGif[1]);
+
+        // this is a public API key - see https://github.com/Giphy/GiphyAPI
+        // we should swap it for a proper key eventually
+        var apiUrl = 'http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s=' + searchTerm;
+
+        request.get(apiUrl, function giphyReceived(err, httpResponse, body) {
+
+            var giphyResponse = JSON.parse(body);
+            var newMarkup;
+
+            if (giphyResponse.data) {
+                newMarkup = '![gif auto-fetched by GitHook-Giphy: "' + wantsGif[1] + '"](' + giphyResponse.data.images.downsized_large + ')';
+            else {
+                '![GitHook-Giphy could not find a gif for "' + wantsGif[1] + '". Have a wilting spoon instead.](https://media.giphy.com/media/MYmFHQ3puVjsQ/giphy.gif)';
+            }
+
+            PAYLOAD.pull_request.body = PAYLOAD.pull_request.body.replace(wantsGif[0], newMarkup);
+
+            var options = {
+                url:      PAYLOAD.pull_request.url,
+                headers: {
+                    'Content-Type':  'application/json',
+                    'User-Agent':    'githook-giphy',
+                    'Authorization': 'token ' + data.access_token
+                },
+                json: {
+                    "body": PAYLOAD.pull_request.body
+                }
+            };
+
+            request.post(options, function templatePosted(err, httpResponse, body) {
+                if (err) {
+                    process.fail('Could not send POST request: ' + err);
+                }
+                else {
+                    process.succeed('Template POST message successful. Response:' + body);
+                }
+            });
+        });
+    }
+    else {
+        process.succeed(PAYLOAD.action + ' (payload action) was not "opened", so there was nothing to do here.');
+    }
+};
