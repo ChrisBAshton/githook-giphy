0eaf96a5d7c286d33d413b96fd86a01513db0650
ChrisBAshton <chrisashtonweb@gmail.com>
2016-10-02 12:21:44 +0100

    new githooks api

--- gif.js
+++ gif.js
@@ -1,17 +1,19 @@
-module.exports = function (data, process) {
+module.exports = function (gh) {
 
-    var request      = require('request'),
-        PAYLOAD      = data.payload,
+    var request      = gh.modules.request,
+        authRequest  = gh.modules.authRequest,
+        PAYLOAD      = gh.data.payload,
         prWasEdited  = PAYLOAD.action === 'opened' || PAYLOAD.action === 'edited',
         wantsGif     = PAYLOAD.pull_request.body.match(/{giphy "(.+)"}/);
 
     if (prWasEdited && wantsGif) {
 
-        var searchTerm = encodeURIComponent(wantsGif[1]);
+        var shortcode     = wantsGif[0],
+            searchTerm    = wantsGif[1];
 
         // this is a public API key - see https://github.com/Giphy/GiphyAPI
         // we should swap it for a proper key eventually
-        var apiUrl = 'http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s=' + searchTerm;
+        var apiUrl = 'http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s=' + encodeURIComponent(searchTerm);
 
         request.get(apiUrl, function giphyReceived(err, httpResponse, body) {
 
@@ -19,37 +21,32 @@ module.exports = function (data, process) {
             var newMarkup;
 
             if (giphyResponse.data) {
-                newMarkup = '![gif auto-fetched by GitHook-Giphy: "' + wantsGif[1] + '"](' + giphyResponse.data.images.downsized_large.url + ')';
+                newMarkup = '![gif auto-fetched by GitHook-Giphy: "' + searchTerm + '"](' + giphyResponse.data.images.downsized_large.url + ')';
             }
             else {
-                '![GitHook-Giphy could not find a gif for "' + wantsGif[1] + '". Have a wilting spoon instead.](https://media.giphy.com/media/MYmFHQ3puVjsQ/giphy.gif)';
+                '![GitHook-Giphy could not find a gif for "' + searchTerm + '". Have a wilting spoon instead.](https://media.giphy.com/media/MYmFHQ3puVjsQ/giphy.gif)';
             }
 
-            PAYLOAD.pull_request.body = PAYLOAD.pull_request.body.replace(wantsGif[0], newMarkup);
+            PAYLOAD.pull_request.body = PAYLOAD.pull_request.body.replace(shortcode, newMarkup);
 
             var options = {
-                url:      PAYLOAD.pull_request.url,
-                headers: {
-                    'Content-Type':  'application/json',
-                    'User-Agent':    'githook-giphy',
-                    'Authorization': 'token ' + data.access_token
-                },
+                url: PAYLOAD.pull_request.url,
                 json: {
                     "body": PAYLOAD.pull_request.body
                 }
             };
 
-            request.post(options, function templatePosted(err, httpResponse, body) {
+            authRequest.post(options, function templatePosted(err, httpResponse, body) {
                 if (err) {
-                    process.fail('Could not send POST request: ' + err);
+                    gh.process.fail('Could not send POST request: ' + err);
                 }
                 else {
-                    process.succeed('Template POST message successful. Response:' + body);
+                    gh.process.succeed('Template POST message successful. Response:' + body);
                 }
             });
         });
     }
     else {
-        process.succeed(PAYLOAD.action + ' (payload action) was not "opened", so there was nothing to do here.');
+        gh.process.succeed(PAYLOAD.action + ' (payload action) was not "opened", so there was nothing to do here.');
     }
 };
